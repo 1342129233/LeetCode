1b8dc30fadaff0f608e66ee538e1455ab031053e
wb_wangxin06 <wb_wangxin06@wb-wangxin06.local>
2021-05-06 15:59:35 +0800

    21.合并两个有序链表

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -49,3 +49,20 @@ function mergeTwoLists(l1, l2) {
     return dummy.next
 }
 mergeTwoLists(l1, l2)
+
+
+// 官方递归
+function mergeTwoListsTwo(l1, l2) {
+    if(l1 === null) {
+        return l2;
+    } else if(l2 === null) {
+        return l1;
+    } else if(l1.val < l2.val) {
+        l1.next = mergeTwoListsTwo(l1.next, l2);
+        return l1
+    } else {
+        l2.next = mergeTwoListsTwo(l1, l2.next);
+        return l2
+    }
+}
+mergeTwoListsTwo(l1, l2)
