09849a1a7f4cfd5b5e9117069bcb655b3eed5cbe
wb_wangxin06 <wb_wangxin06@wb-wangxin06.local>
2021-05-10 14:46:42 +0800

    53.最大子序和

--- 53.最大子序和.js
+++ 53.最大子序和.js
@@ -3,33 +3,17 @@
 let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]   // 输出 6   连续子数组[4, -1, 2, 1]
 function maxSubArray(nums) {
     const memo = [];
-    memo[0] = nums[0];
-    let max = nums[0];
+    memo[0] = nums[0];    // 每个值大小  数组形式找出最大值，对象形式找出哪几个值最大（使用 new map()）
+    let max = nums[0];    // 最大值
     let maxkey = 0
     let a = []
     let list = 0
 
     for(let i=1; i < nums.length; i++) {
+        // 比较当前的值大，还是加下一个值大，赋值给下标的值
         memo[i] = Math.max(nums[i] + memo[i - 1], nums[i]);    // 最大值
         // console.log(memo)
         max = Math.max(max, memo[i])
-    }
-
-    // 下面是自己找出是哪几个数字的和，你在范围
-    maxkey = memo.indexOf(max)  // 找到最大值数组最后一个下标，第几次值最大
-    for(let i = max; i > 0; i--) {
-        if(nums[i] === max) {
-            a.push(nums[i])
-            break;
-        }
-        
-        if(list === max) {
-            console.log(a);
-            break;
-        } else {
-            list = list + nums[i]
-            a.push(nums[i])
-        }
     }
     return max
 }
